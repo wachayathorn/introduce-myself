c56c96298e10be67853195f2e40d7cf5c771c7b7
night <wachayathorn>
2025-12-12 21:06:19 +0700

    fix navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -3,11 +3,11 @@ export default function Navbar() {
     <div className="bg-[#FAFAFA]">
       <nav className="container mx-auto max-w-7xl h-auto p-5 flex flex-col items-center md:flex-row md:justify-center">
         <ul className="flex flex-col md:flex-row">
-          <li className="mx-10 text-center"><span className="cursor-pointer">Home</span></li>
-          <li className="mx-10 text-center"><span className="cursor-pointer">Experience</span></li>
-          <li className="mx-10 text-center"><span className="cursor-pointer">Education</span></li>
-          <li className="mx-10 text-center"><span className="cursor-pointer">Skill</span></li>
-          <li className="mx-10 text-center"><span className="cursor-pointer">Contact</span></li>
+          <li className="mx-10 text-center"><span className="cursor-pointer hover:font-extrabold">HOME</span></li>
+          <li className="mx-10 text-center"><span className="cursor-pointer hover:font-extrabold">EXPERIENCE</span></li>
+          <li className="mx-10 text-center"><span className="cursor-pointer hover:font-extrabold">EDUCATION</span></li>
+          <li className="mx-10 text-center"><span className="cursor-pointer hover:font-extrabold">SKILL</span></li>
+          <li className="mx-10 text-center"><span className="cursor-pointer hover:font-extrabold">CONTACT</span></li>
         </ul>
       </nav>
     </div>
