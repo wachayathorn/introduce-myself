e4f0501e28566e117d4a8fd111d3571453586133
night <wachayathorn.s>
2025-12-20 21:19:50 +0700

    update experience

--- app/data/experiences.tsx
+++ app/data/experiences.tsx
@@ -7,14 +7,6 @@ export const experiences = [
     location: "Nonthaburi, Thailand",
   },
   {
-    id: 2,
-    title: "Backend Developer",
-    company: "Superdev Co., Ltd",
-    period: "01/2023 - 12/2025",
-    location: "Bangkok, Thailand",
-    employmentType: "Part-time",
-  },
-  {
     id: 3,
     title: "Backend Developer",
     company: "RV Connex Co., Ltd",
